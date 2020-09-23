49a13515212ebafe2ac9c2339f8417c67bee7d3d
FGDevs <fitrakadev@gmail.com>
2020-09-23 08:39:19 +0700

    Adding font-family and Install react-gsap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
